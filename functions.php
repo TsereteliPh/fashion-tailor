998be0105985250f4f3e5b91bf219fe0b019ec0a
TsereteliPh <filipptsereteli@yandex.ru>
2024-07-27 13:14:11 +0300

    add and done .header

--- functions.php
+++ functions.php
@@ -27,6 +27,7 @@ if ( ! function_exists( 'adem_setup' ) ) {
 		register_nav_menus(
 			array(
 				'menu_main' => 'Основное меню',
+				'footer_menu' => 'Меню футера'
 			)
 		);
 	}
--- header.php
+++ header.php
@@ -11,7 +11,86 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
+<?php
+	$tel = get_field( 'tel', 'options' );
+	$socials = get_field( 'socials', 'options' );
+?>
 <header class="header">
+	<div class="container">
+		<div class="header__panel">
+			<a href="<?php echo bloginfo( 'url' ); ?>" class="header__logo" aria-label="Логотип компании Fashion Tailor">
+				<div class="logo"></div>
 
+				Производство спортивной одежды
+			</a>
+
+			<button class="header__catalog" type="button">
+				<span></span>
+			</button>
+
+			<?php wp_nav_menu( array(
+				'theme_location' => 'menu_main',
+				'container' => '',
+				'menu_id' => 'menu-main',
+				'menu_class' => 'reset-list header__menu'
+			) ); ?>
+
+			<?php if ( $tel || $options ) : ?>
+				<div class="contacts header__contacts">
+					<?php if ( $tel ) : ?>
+						<a href="tel:<?php echo preg_replace( '/[^0-9,+]/', '', $tel ); ?>" class="contacts__tel"><?php echo $tel; ?></a>
+					<?php endif; ?>
+
+					<?php if ( $socials ) : ?>
+						<div class="contacts__socials">
+							<?php foreach ( $socials as $social ) : ?>
+								<a href="<?php echo $social['link']; ?>" class="contacts__social">
+									<svg width="20" height="20"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-<?php echo $social['social']; ?>"></use></svg>
+									<?php echo $social['label']; ?>
+								</a>
+							<?php endforeach; ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			<?php endif; ?>
+
+			<button class="btn-underline header__callback" type="button" data-fancybox data-src="<?php //TODO: callback modal ?>">Заказать звонок</button>
+
+			<button class="header__burger" type="button" aria-label="Открыть/Закрыть меню">
+				<span></span>
+			</button>
+		</div>
+
+		<div class="header__drop">
+			<?php wp_nav_menu( array(
+				'theme_location' => 'menu_main',
+				'container' => '',
+				'menu_id' => 'menu-main',
+				'menu_class' => 'reset-list header__drop-menu'
+			) ); ?>
+
+			<?php if ( $tel || $options ) : ?>
+				<div class="contacts header__drop-contacts">
+					<?php if ( $tel ) : ?>
+						<a href="tel:<?php echo preg_replace( '/[^0-9,+]/', '', $tel ); ?>" class="contacts__tel"><?php echo $tel; ?></a>
+					<?php endif; ?>
+
+					<?php if ( $socials ) : ?>
+						<div class="contacts__socials">
+							<?php foreach ( $socials as $social ) : ?>
+								<a href="<?php echo $social['link']; ?>" class="contacts__social">
+									<svg width="20" height="20"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-<?php echo $social['social']; ?>"></use></svg>
+									<?php echo $social['label']; ?>
+								</a>
+							<?php endforeach; ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			<?php endif; ?>
+
+			<button class="btn-underline header__drop-callback" type="button" data-fancybox data-src="<?php //TODO: callback modal ?>">Заказать звонок</button>
+		</div>
+	</div>
 </header>
+
 <main class="main">
