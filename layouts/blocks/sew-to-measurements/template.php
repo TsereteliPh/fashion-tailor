f2c578f6311cab40bf303590a8a0f409f54995da
TsereteliPh <filipptsereteli@yandex.ru>
2024-08-01 14:53:59 +0300

    add and done .sew-to-measurements

---
+++ layouts/blocks/sew-to-measurements/template.php
@@ -0,0 +1,46 @@
+<section class="sew-to-measurements">
+	<div class="container">
+		<?php get_template_part( '/layouts/partials/title', null, array(
+			'class' => 'sew-to-measurements__title',
+			'title' => get_sub_field( 'title' )
+		) ); ?>
+
+		<?php if ( get_sub_field( 'text' ) ) : ?>
+			<div class="sew-to-measurements__text">
+				<svg width="18" height="18"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow-diagonal"></use></svg>
+				<?php the_sub_field( 'text' ); ?>
+			</div>
+		<?php endif; ?>
+
+		<?php
+			$advantages = get_sub_field( 'advantages' );
+			if ( $advantages ) :
+				?>
+
+				<div class="sew-to-measurements__box">
+					<ul class="reset-list sew-to-measurements__list">
+						<?php foreach ( $advantages as $advantage ) : ?>
+							<li class="sew-to-measurements__item">
+								<div class="sew-to-measurements__item-icon">
+									<?php echo wp_get_attachment_image( $advantage['icon'], 'medium', false ); ?>
+								</div>
+
+								<div class="sew-to-measurements__item-label"><?php echo $advantage['label']; ?></div>
+
+								<div class="sew-to-measurements__item-text"><?php echo $advantage['text']; ?></div>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+
+					<button class="btn sew-to-measurements__download" type="button">
+						<?php //TODO download btn ?>
+						Скачать весь каталог
+						<svg width="18" height="18"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow-diagonal"></use></svg>
+					</button>
+				</div>
+
+				<?php
+			endif;
+		?>
+	</div>
+</section>
